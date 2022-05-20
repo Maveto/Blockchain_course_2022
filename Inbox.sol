9cca3061adbf5b1a0f0ac103b45aa0c48daefb27
Maveto <maveto13@gmail.com>
2022-05-20 13:09:32 -0400

    Tarea 1

---
+++ Inbox.sol
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract Inbox {
+
+    string public message;
+
+    constructor(){
+        
+    }
+
+    function getMessage() public view returns(string memory) {
+        return message;
+    }
+
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+
+}
