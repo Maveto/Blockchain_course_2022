bd740e11d829e45a56510d61c1955b7795b1116e
Maveto <maveto13@gmail.com>
2022-06-20 11:02:49 -0400

    Tarea 1: React, firebase y truffle

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { connectWallet, initialize } from './ethereum/web3';
+import contractLottery from './ethereum/abis/Lottery.json';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
